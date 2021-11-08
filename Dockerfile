79ff5ef1cee1b975db4267a98727f379f72b6698
Shankha <shankhashubhrasarkar@gmail.com>
2021-11-08 13:33:40 +0530

    main

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# pull official base image
+FROM node:13.12.0-alpine
+
+#SET WORKING DIRECTORY
+WORKDIR /app
+
+ENV PATH /app/node_modules/.bin:$PATH
+
+COPY package.json ./
+
+COPY package-lock.json ./
+
+RUN npm install --silent
+
+
+#add app
+COPY . ./
+
+#start app
+CMD ["npm", "start"]
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Simple Dockerized React App Building. XYZ
         </p>
         <a
           className="App-link"
